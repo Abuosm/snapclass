4dbb88611c6b56cfd1b1531a13449fd378883566
Abuosm <abubakerosman56@gmail.com>
2026-05-09 01:44:39 +0530

    Initial commit

---
+++ src/components/footer.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+def footer_home():
+    st.markdown("""
+        <div style="
+            margin-top:2rem;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            font-weight:bold;
+            font-size:18px;
+            color:#0E1117;
+        ">
+            🚀 Created with  by Abubakar Osman
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    st.markdown("""
+        <div style="
+            margin-top:2rem;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            font-weight:bold;
+            font-size:18px;
+            color:#0E1117;
+        ">
+            🚀 Created with  by Abubakar Osman
+        </div>
+    """, unsafe_allow_html=True)
